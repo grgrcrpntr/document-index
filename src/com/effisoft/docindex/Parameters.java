099eb78eecdc5c845cbf264e02865fa6bf67453b
gcarpentier <gcarpentier@LPT-FR-11.effisoft-group.com>
2019-01-08 23:06:33 +0100

    Handling file type black/white list

--- src/com/effisoft/docindex/Parameters.java
+++ src/com/effisoft/docindex/Parameters.java
@@ -1,5 +1,8 @@
 package com.effisoft.docindex;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /** Parameter handling class
  */
 public class Parameters {
@@ -12,10 +15,10 @@ public class Parameters {
 	public String _rootpath;
 	/** Path of the output HTML index file */
 	public String _outfile;
-	/** True if PDF files are allowed in index */
-	public boolean _allowPDF;
-	/** True if non PDF files are allowed in index */
-	public boolean _allowNonPDF;
+	/** True is allowed file types are whitelisted */
+	private boolean _whitelist;
+	/** IF _whitelist THEN allowed file types. ELSE blacklisted types. */
+	private Set<String> _typeList;
 		
 		// ----------------------
 		// Constructor
@@ -26,20 +29,67 @@ public class Parameters {
 		*/
 		public Parameters(String[] args) {
 			
+			_whitelist = false;
+			_typeList = new HashSet<String>();
+			
+			if (args.length < 1) {
+				throw new IllegalArgumentException("Error: Input direstory is not specified.");
+			}
+			
+			if (args.length < 2) {
+				throw new IllegalArgumentException("Error: Output file is not specified.");
+			}
+			
 			// In and Out paths
 			_rootpath = args[0];
 			_outfile = _rootpath + "\\" + args[1];
 			
-			// Default booleans
-			_allowPDF = true;
-			_allowNonPDF = true;
+			if (args.length > 2) {
+				
+				// Choose whithlist or blacklist file type handling
+				switch (args[2]) {
+				case "-include":
+					_whitelist = true;
+					break;
+				case "-exclude":
+					_whitelist = false;
+					break;
+				default:
+					throw new IllegalArgumentException("Error: " + args[2] + " is not a valid option.");
+				}
+				
+				if (args.length < 4) {
+					throw new IllegalArgumentException("Error: " + args[2] + " option requires a file type list.");
+				}
+				
+				// Create file type white/black list
+				String[] extensions = args[3].split("/");
+				for (String ext : extensions) _typeList.add(ext);
+				
+			}	
+		}
+		
+		// ----------------------
+		// Public methods
+		// ----------------------
+		
+		/** Return true if file type is allowed in index
+		* @param extension	(String) File type
+		* @return boolean	
+		*/
+		public boolean isFileTypeAllowed(String extension) {
 			
-			// Handling PDF options
-			if (args.length == 3) {
-				if (args[2].equals("-nopdf")) _allowPDF = false;
-				if (args[2].equals("-onlypdf")) _allowNonPDF = false;
+			boolean out = false;
+			
+			if (_whitelist) {
+				if (_typeList.contains(extension)) 
+					return true;
+			} else {
+				if (!_typeList.contains(extension))
+					return true;
 			}
 			
+			return out;
 		}
 		
 }
